e52038c4b8bd11defcf467339b0077901999205e
Ikinidae <federicaelia24@gmail.com>
2022-06-07 16:38:46 +0200

    Lavoro completo (credo)

---
+++ src/components/FirstTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'FirstTitle',
+    // props: {
+    //     // msg: String
+    // }
+    }
+</script>
+
+<style scoped>
+
+</style>
